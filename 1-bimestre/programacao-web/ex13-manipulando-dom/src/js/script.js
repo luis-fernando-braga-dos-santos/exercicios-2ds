3e98f04a6c12790ba2e9d262a64410ce89755884
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2026-03-30 15:58:01 +0000

    ultimo esforço

--- 1-bimestre/programacao-web/ex13-manipulando-dom/src/js/script.js
+++ 1-bimestre/programacao-web/ex13-manipulando-dom/src/js/script.js
@@ -1,6 +1,8 @@
 const mensagem = document.getElementById("mensagem");
 const alterar = document.getElementById("alterar");
 
-alterar.addEventListener("click", function() {
+if (mensagem && alterar) {
+  alterar.addEventListener("click", function () {
     mensagem.textContent = "Mensagem alterada com JavaScript";
-});
+  });
+}
--- 1-bimestre/programacao-web/ex13-manipulando-dom/test/index.test.js
+++ 1-bimestre/programacao-web/ex13-manipulando-dom/test/index.test.js
@@ -1,6 +1,14 @@
-const mensagem = document.getElementById("mensagem");
-const botao = document.getElementById("alterar");
+const fs = require("fs");
+const html = fs.readFileSync("index.html", "utf8");
 
-botao.addEventListener("click", function () {
-  mensagem.textContent = "Mensagem alterada com JavaScript";
+test("Existe parágrafo com id mensagem", () => {
+  expect(html).toContain('id="mensagem"');
+});
+
+test("Existe botão alterar", () => {
+  expect(html).toContain('id="alterar"');
+});
+
+test("Script conectado corretamente", () => {
+  expect(html).toContain("./src/js/script.js");
 });
