21609fa1e04ea62d95b78970679e3d9cf95fe8cb
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2026-04-28 14:20:56 +0000

    exercicio 2

--- 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
+++ 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
@@ -1,9 +1,11 @@
 export default function App(){
 const filmeFavorito = "Homem Aranha"
+const MatériaFavorita = "desenvovimento de sistemas"
   return(
       <div>
         <h1>Luis Fernando Braga dos Santos</h1>
         <p>Meu filme favorito é: {filmeFavorito}</p>
+        <p>Minha matéria favorita é: {MatériaFavorita}</p>
       </div>
   )
 }
