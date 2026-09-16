76256253f15efbf6e96ce96de69b7050475e765d
luis-fernando-braga-dos-santos <luis.santos295@aluno.ce.gov.br>
2026-09-16 11:52:21 +0000

    projeto 3 based

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
@@ -2,7 +2,25 @@ const Notificacao = require("./Notificacao")
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
 
+const { Email, SMS, App } = require("./Notificacao")
 
+class Factory {
+    static criar(tipo, mensagem) {
+        if (tipo === "email") {
+            return new Email(mensagem)
+        }
+
+        if (tipo === "sms") {
+            return new SMS(mensagem)
+        }
+
+        if (tipo === "app") {
+            return new App(mensagem)
+        }
+
+        throw new Error("Tipo de notificação inválido")
+    }
+}
 
 
 
